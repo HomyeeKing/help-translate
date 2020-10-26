ceeff5776cd3fe32fdf1f9bce225a257548cee7a
wanghongye <wanghongye@kuaishou.com>
2020-10-26 14:57:01 +0800

    feat: add cache and onRemoved life cycle

--- background.js
+++ background.js
@@ -1,12 +1,30 @@
 'use strict';
 const XL = 'XL',
-	NORMAL = 'NORMAL';
+    NORMAL = 'NORMAL',
+    handled_tabs = new Map();
 let DEVICE_TYPE = NORMAL,
-	active_tabs = [];
+    active_tabs = [];
 
+    /**已被处理过的tab页的id
+     * 
+     * key:tabId
+     * value:windowId //用于判断是否仍处于当前窗口
+     */
+function isHandledTabs(tabId,windowId){
+    if(handled_tabs.has(tabId) && handled_tabs.get(tabId) === windowId)return true
+    else{
+        handled_tabs.set(tabId,windowId)
+         return false
+    }
+}
+
+function clear_removed_tab(tabId){
+    handled_tabs.delete(tabId)
+}
 
 chrome.tabs.onActivated.addListener(function (selectionInfo) {
-	const { windowId, tabId } = selectionInfo;
+    const { windowId, tabId } = selectionInfo;
+    if(isHandledTabs(tabId,windowId))return
 	chrome.tabs.query({ windowId }, (tabs) => {
 		if (tabs[0].width > 1440) {
 			chrome.tabs.setZoom(tabId, 1.25);
@@ -17,6 +35,7 @@ chrome.tabs.onActivated.addListener(function (selectionInfo) {
 });
 chrome.tabs.onMoved.addListener(function (tabId, moveInfo) {
 	const { windowId } = moveInfo;
+    if(isHandledTabs(tabId,windowId))return
 
 	chrome.tabs.query({ windowId }, (tabs) => {
 		if (tabs[0].width > 1440) {
@@ -28,10 +47,15 @@ chrome.tabs.onMoved.addListener(function (tabId, moveInfo) {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	const { width, id } = tab;
+    const { width,windowId } = tab;
+    if(isHandledTabs(tabId,windowId))return
 	if (width > 1440) {
-		chrome.tabs.setZoom(id, 1.25);
+		chrome.tabs.setZoom(tabId, 1.25);
 	} else {
-		chrome.tabs.setZoom(id, 1);
+		chrome.tabs.setZoom(tabId, 1);
 	}
 });
+
+chrome.tabs.onRemoved.addListener((tabId)=>{
+    clear_removed_tab(tabId)
+})
