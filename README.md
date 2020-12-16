899e5651523b0c7dca57e774a2a82e83fe8a997c
wanghongye <wanghongye@kuaishou.com>
2020-12-16 10:38:54 +0800

    feat: block by create result panel in where cursor is

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 - [English](./README_EN.md)
 
-# 根据浏览器窗口适配页面大小🚀🚀
+# 根据浏览器窗口适配页面大小 🚀🚀
 
 > 仅限谷歌浏览器
 
 ### 使用方法：
 
 把文件克隆到本地
+
 ```git
  git clone git@github.com:HomyeeKing/chrome-extension_zoom.git
 ```
@@ -16,4 +17,5 @@
 - 然后选择从仓库克隆下来的文件夹
 - 不出意外，应该就可以使用了
 
-有问题欢迎issue~
+学习博客参考:https://juejin.cn/post/6844904077889912839
+有问题欢迎 issue~
--- background.js
+++ background.js
@@ -1,38 +1,57 @@
 "use strict";
 document.write("<script  src='./md5.js'></script>");
 document.write("<script  src='./xhr.js'></script>");
-
-chrome.contextMenus.create({
-	id: Date.now() + "",
-	title: "翻译 %s",
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.contextMenus.create({
+		id: Date.now() + "",
+		title: "翻译 %s",
+		contexts: ["selection"],
+	});
+	chrome.contextMenus.onClicked.addListener(async ({ selectionText }) => {
+		chrome.contextMenus.removeAll();
+		var appid = "20200430000435396";
+		var key = "iQiMeAAafmYSaPxDcN0h";
+		var salt = Date.now() + "";
+		var query = selectionText;
+		var from = "auto";
+		var to = "zh";
+		var str1 = appid + query + salt + key;
+		var sign = MD5(str1);
+		const res = await postData(
+			"https://fanyi-api.baidu.com/api/trans/vip/translate",
+			{
+				q: query,
+				appid,
+				salt,
+				from,
+				to,
+				sign,
+			}
+		);
+		showNotifications();
+	});
 });
-chrome.contextMenus.onClicked.addListener(async ({ selectionText }) => {
-	chrome.contextMenus.removeAll();
-	var appid = "20200430000435396";
-	var key = "iQiMeAAafmYSaPxDcN0h";
-	var salt = Date.now() + "";
-	var query = selectionText;
-	var from = "auto";
-	var to = "zh";
-	var str1 = appid + query + salt + key;
-	var sign = MD5(str1);
-	const res = await postData(
-		"https://fanyi-api.baidu.com/api/trans/vip/translate",
-		{
-			q: query,
-			appid,
-			salt,
-			from,
-			to,
-			sign,
-		}
-	);
-	console.log(res);
-	const div = document.createElement("div");
-	div.textContent = "asdasd";
-	document.documentElement.appendChild(div);
-});
 
+function showNotifications() {
+	const opt = {
+		title: "翻译结果",
+		message: "msg",
+		iconUrl: "images/dog-128.png",
+		type: "list",
+		// 文字列表
+		items: [
+			{ title: "1.", message: "五点半该下班了" },
+			{ title: "2.", message: "记得按时吃饭" },
+		],
+		//按钮功能,设置标题和图片
+		buttons: [
+			{ title: "call", iconUrl: "images/dog-16.png" },
+			{ title: "email", iconUrl: "images/dog-16.png" },
+		],
+	};
+	//创建并显示
+	chrome.notifications.create(opt);
+}
 function postData(url, data) {
 	// Default options are marked with *
 	return fetch(url, {
